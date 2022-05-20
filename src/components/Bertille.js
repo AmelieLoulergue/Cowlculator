a50d50a52252c2b48b7c213e6e013da748859ebc
bertillesmith <bertilled.smith@gmail.com>
2022-05-20 18:46:20 -0400

    unsuccessful test again

--- src/components/Bertille.js
+++ src/components/Bertille.js
@@ -1,8 +1,45 @@
-import React from "react";
+//import React from "react";
 
-function Bertille() {
-  let test = "Bertille";
-  return <div>{test} : Modification du fichier</div>;
-}
+//function Bertille() {
+  //let test = "Bertille";
+  //return <div>{test} : Modification du fichier</div>;
+//}
 
-export default Bertille;
+//export default Bertille;
+
+
+// time function
+
+function funcTime(data){
+  
+  //questionnaire answers (start date and finish date) needs to be tranformed into a date format if not alread
+      //start <- format(as.Date(data[[1]], format = "%m/%d/%Y"), "%Y-%m-%d")
+      //finish <-format(as.Date(data[[2]], format = "%m/%d/%Y"), "%Y-%m-%d")
+      /* time period in years (see if conversion needed, ex in R, difftime gave the time difference 
+      in week so I add to divide it by 52.25 to have it in year). Depend on the variable that we have
+      from the questionnaire */
+      //let time_period <-as.numeric(difftime(finish, start, units = "weeks"))/52.25
+//let time_period= finish-time
+  //return(time_period)
+//}
+
+
+// Electricity function
+
+//function funElectricity(data){
+  //data traitment (turn into numeric) if needed
+  // electricity consumption
+ // let cons= //cons data from questionnaire
+  // electricity production
+  //if (prod question) {
+  //  let prod = //prod data from questionnaire
+ // } else {
+    // prod=0
+  //}
+//let total= cons=prod
+
+// multiplication by coeff associated to state
+//let state= // state from questionnaire
+//let coeffState= // coeff associated with state in the coeff file
+
+ } 
