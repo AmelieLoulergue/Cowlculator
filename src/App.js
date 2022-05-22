8613ac77f7f2c085e47818801f20c4e552dd06db
bertillesmith <bertilled.smith@gmail.com>
2022-05-22 09:20:39 -0400

    add excel coefficients and test function time

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Bertille from "./components/Bertille";
+import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/bertille" element={<Bertille />}></Route>
+          <Route path="/form" element={<Form />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/utils/calculs.js
@@ -0,0 +1,39 @@
+import React, { startTransition } from "react";
+
+function calculs(datasForm) {
+  console.log("toutes les datas", datasForm);
+  console.log("date de fin", datasForm.endDate);
+  console.log("date de début", datasForm.startDate);
+  console.log(
+    "nombre de bisons matures",
+    datasForm.farm_american_bison_matur_numb.value
+  );
+  console.log(
+    "natgas unit Therm",
+    datasForm.natgas_unit[0].value,
+    datasForm.natgas_unit[0].selected
+  );
+  // tu peux boucler sur des tableaux
+  datasForm.natgas_unit.map((unit, index) => {
+    console.log(unit.value, unit.selected, index);
+  });
+  // tu peux trouver un élement avec une condition
+  const unit_selected = datasForm.natgas_unit.find(
+    (unit) => unit.selected === true
+  );
+  console.log(unit_selected);
+
+  let test= funcTime(datasForm)
+  console.log(test)
+}
+
+export default calculs;
+
+//function time
+function funcTime(datasForm){
+  let start= datasForm.startDate
+  let end = datasForm.endDate
+  let time = end-start
+
+  return time
+}
