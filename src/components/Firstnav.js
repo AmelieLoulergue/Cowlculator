eaffcfde9e785a5552030ac5440ed48d75140908
Clément L <clement.loulergue@live.fr>
2022-06-01 23:14:05 +0200

    Improved Home page + Getstarted page begin

--- src/components/Firstnav.js
+++ src/components/Firstnav.js
@@ -1,20 +1,28 @@
 import * as React from "react";
 import './Firstnav.css';
+import { useNavigate } from "react-router-dom";
 import logow from '../assets/img/logo-w.png';
-import account from '../assets/img/account.png';
+import account from '../assets/svg/profile.svg';
 
-function Firstnav() {
+function Firstnav({viewHeight, scroll}) {
+    let navigate = useNavigate();
+    function onClickNavigate(e) {
+        e.preventDefault();
 
-  return (
-    <>
-    <nav id="Firstnav">
-        <ul>
-            <li><img src={logow} id="logo"></img></li>
-            <li><img src={account}></img></li>
-        </ul>
-    </nav>
-    </>
-  );
+        navigate('/getstarted')
+    }
+        return (
+            <>
+            <nav id="Firstnav" className={scroll > 0.9*viewHeight-94 ? "Firstnav-bg" : ""}>
+                <ul>
+                    <li><img src={logow} id="logo" alt="Logo"></img></li>
+                    <li onClick={onClickNavigate}><img src={account} alt="profile icon for login-in"></img></li>
+                </ul>
+            </nav>
+            </>
+          );
+    
+  
 }
 
 export default Firstnav;
