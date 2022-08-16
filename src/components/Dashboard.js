4ecae61067f93a7762431fdaeb647454f48b843a
Clément L <clement.loulergue@live.fr>
2022-08-16 19:02:31 +0200

    new dashboard and nav sidebar et top bar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import './Dashboard.css';
 
-import charts from '../assets/svg/charts.svg'
+import charts from '../assets/svg/charts.svg';
+import logow from '../assets/img/logo-w.png';
 
 import Bg from './Bg';
 
@@ -9,25 +10,37 @@ function Dashboard() {
     return (
       <>
       <div id="dash">
-        <div className="dash-nav">
-            <div className="dash-profile">
-                <h1>
-                    Dashboard
-                </h1>
-                <div>
-                    <img></img>
-                    <h2>Username</h2>
-                    <p>Project name</p>
+        <div className="dash-side slideLeft">
+            <div className="dash-side-container slideLeft">
+                <div className="dash-profile">
+                    <h1>
+                        Dashboard
+                    </h1>
+                    <div>
+                        <img></img>
+                        <h2>Username</h2>
+                        <p>Project name</p>
+                    </div>
+                </div>
+                <div className="dash-li">
+                    <ul>
+                        <li><img src={charts}></img> Menu 1 </li>
+                        <li><img src={charts}></img> Menu 2 </li>
+                        <li><img src={charts}></img> Menu 3 </li>
+                        <li><img src={charts}></img> Menu 4 </li>
+                    </ul>
                 </div>
             </div>
-            <div className="dash-li">
-                <ul>
-                    <li><img src={charts}></img> Menu 1 </li>
-                    <li><img src={charts}></img> Menu 2 </li>
-                    <li><img src={charts}></img> Menu 3 </li>
-                    <li><img src={charts}></img> Menu 4 </li>
-                </ul>
+        </div>
+
+        <div className="dash-nav">
+            <div className="dash-nav-icon">
+                <span></span>
             </div>
+            <div className="logo">
+                CarbonBalance <img src={logow}></img>
+            </div>
+            <div></div>
         </div>
       </div>
       <Bg></Bg>
