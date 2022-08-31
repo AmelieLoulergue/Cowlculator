9423afdd12d94e39b91733f5a2758466ccb55911
AmelieLoulergue <loulergueamelie@yahoo.fr>
2022-08-31 10:57:36 +0200

    WIP

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -12,33 +12,44 @@ import send from "../assets/svg/send.svg";
 import menu from "../assets/svg/burger.svg";
 import Bg from "./Bg";
 import counterQuestions from "../utils/counterQuestions";
+
 const NewForm = () => {
   const [numberOfResponse, setNumberOfResponse] = useState(0);
   const [blocIndex, setBlocIndex] = useState(0);
-  let counter = 0;
-  const [totalCounterBloc, setTotalCounterBloc] = useState(0);
+  const [questionToDisplay, setQuestionToDisplay] = useState(null);
+  const [displayedQuestions, setDisplayedQuestions] = useState([]);
 
-  const [numberOfResponseBloc, setNumberOfResponseBloc] = useState(0);
-  const [counterBlocLimits, setCounterBlocLimits] = useState(
-    listOfQuestions.formQuestions.map((bloc, bloc_index) =>
-      counterQuestions({ bloc, bloc_index })
-    )
-  );
-  const startForm = () => {};
-  useEffect(() => {
-    const sum = counterBlocLimits
-      .filter((element) => Number(element.id.split("bloc_")[1]) <= blocIndex)
-      .reduce((accumulator, object) => {
-        return accumulator + object.counter;
-      }, 0);
-    setTotalCounterBloc(sum);
-  }, [numberOfResponse, blocIndex]);
-  useEffect(() => {
-    if (blocIndex !== 0) {
-      setNumberOfResponseBloc(0);
+  console.log({ questionToDisplay, displayedQuestions });
+  const renderQuestion = () => {
+    let question = listOfQuestions.formQuestions
+      .find((bloc) => bloc.bloc_id === `bloc_${blocIndex}`)
+      .questions.find(
+        (question) => displayedQuestions.length === question.index_id
+      );
+
+    if (
+      !question &&
+      listOfQuestions.formQuestions.find(
+        (bloc) => bloc.bloc_id === `bloc_${blocIndex}`
+      ).bloc_questions_number === numberOfResponse
+    ) {
+      setBlocIndex(blocIndex + 1);
+      question = listOfQuestions.formQuestions
+        .find((bloc) => bloc.bloc_id === `bloc_${blocIndex + 1}`)
+        .questions.find(
+          (question) => displayedQuestions.length === question.index_id
+        );
     }
-  }, [totalCounterBloc]);
-
+    console.log(question);
+    setQuestionToDisplay(question);
+  };
+  useEffect(() => {
+    questionToDisplay &&
+      setDisplayedQuestions([...displayedQuestions, questionToDisplay]);
+  }, [numberOfResponse]);
+  useEffect(() => {
+    questionToDisplay && renderQuestion();
+  }, [displayedQuestions]);
   return (
     <div className="">
       <div className="formChat">
@@ -52,190 +63,68 @@ const NewForm = () => {
           </div>
           <div className="btns">
             <img src={back_arrow} alt=""></img>
-            <button className="btn" onClick={startForm}>
+            <button className="btn" onClick={() => renderQuestion()}>
               Let's get started
             </button>
             <img src={home} alt=""></img>
           </div>
         </div>
-        <div className="questions">
-          {listOfQuestions.formQuestions.map((bloc, key_bloc) => (
+        <div id="questions-form" className="questions">
+          {displayedQuestions.length > 0 &&
+            displayedQuestions.map((question) => (
+              <>
+                {question.bloc_name && question.bloc_name.replace("_", " ")}
+                <div
+                  key={question.question_id}
+                  id={question.question_id}
+                  className={
+                    question.question_class ? question.question_class : ""
+                  }
+                >
+                  <RenderQuestion
+                    numberOfResponse={numberOfResponse}
+                    setNumberOfResponse={setNumberOfResponse}
+                    question={question}
+                    blocIndex={blocIndex}
+                    setBlocIndex={setBlocIndex}
+                    hasResponse={true}
+                  />
+                </div>
+              </>
+            ))}
+          {questionToDisplay && (
+            <>
+              {questionToDisplay.bloc_name &&
+                questionToDisplay.bloc_name.replace("_", " ")}
+              <div
+                key={questionToDisplay.question_id}
+                id={questionToDisplay.question_id}
+              >
+                <RenderQuestion
+                  numberOfResponse={numberOfResponse}
+                  setNumberOfResponse={setNumberOfResponse}
+                  question={questionToDisplay}
+                  blocIndex={blocIndex}
+                  setBlocIndex={setBlocIndex}
+                />
+              </div>
+            </>
+          )}
+          {/*  On parcours tous les blocs existants dans listOfQuestions un à un, et on cache avec is-hidden selon l'étape de l'utilisateur */}
+          {/* {listOfQuestions.formQuestions.map((bloc, key_bloc) => (
             <div
               key={`${key_bloc}`}
-              id={`bloc_${key_bloc}`}
+              id={bloc.bloc_id}
               className={key_bloc <= blocIndex ? "" : "is-hidden"}
             >
               <div className="nav">
                 <img src={menu} alt=""></img>
-                <h1>{bloc.bloc_id}</h1>
+                <h1>{bloc.bloc_name.replace("_", " ")}</h1>
                 <img src={home} alt=""></img>
               </div>
-              {bloc.questions.map((question, key_question) => {
-                counter += 1;
-                return (
-                  <div key={`range_1_${counter}`}>
-                    <div key={`question_${counter}`} id={`question_${counter}`}>
-                      <RenderQuestion
-                        counterBlocLimits={counterBlocLimits}
-                        numberOfResponse={numberOfResponse}
-                        setNumberOfResponse={setNumberOfResponse}
-                        question={question}
-                        range={0}
-                        bloc_id={key_bloc}
-                        questionIndex={key_question}
-                        blocIndex={blocIndex}
-                        counter={counter}
-                        totalCounterBloc={totalCounterBloc}
-                        setBlocIndex={setBlocIndex}
-                        numberOfResponseBloc={numberOfResponseBloc}
-                        setNumberOfResponseBloc={setNumberOfResponseBloc}
-                      />
-                    </div>
-                    {question.sub_questions &&
-                      question.sub_questions.length > 0 &&
-                      question.sub_questions.map(
-                        (sub_question_1, key_sub_question_1) => {
-                          counter += 1;
-                          return (
-                            <div key={`range_2_${counter}`}>
-                              {" "}
-                              <div
-                                key={`question_${counter}`}
-                                id={`question_${counter}`}
-                              >
-                                <RenderQuestion
-                                  counterBlocLimits={counterBlocLimits}
-                                  numberOfResponse={numberOfResponse}
-                                  setNumberOfResponse={setNumberOfResponse}
-                                  sup_question={question}
-                                  question={sub_question_1}
-                                  range={1}
-                                  totalCounterBloc={totalCounterBloc}
-                                  bloc_id={key_bloc}
-                                  questionIndex={
-                                    key_question + key_sub_question_1
-                                  }
-                                  blocIndex={blocIndex}
-                                  counter={counter}
-                                  setBlocIndex={setBlocIndex}
-                                  numberOfResponseBloc={numberOfResponseBloc}
-                                  setNumberOfResponseBloc={
-                                    setNumberOfResponseBloc
-                                  }
-                                />
-                              </div>
-                              {sub_question_1.sub_questions &&
-                                sub_question_1.sub_questions.length > 0 &&
-                                sub_question_1.sub_questions.map(
-                                  (sub_question_2, key_sub_question_2) => {
-                                    counter += 1;
-                                    return (
-                                      <div key={`range_3_${counter}`}>
-                                        {" "}
-                                        <div
-                                          key={`question_${counter}`}
-                                          id={`question_${counter}`}
-                                        >
-                                          <RenderQuestion
-                                            counterBlocLimits={
-                                              counterBlocLimits
-                                            }
-                                            numberOfResponse={numberOfResponse}
-                                            setNumberOfResponse={
-                                              setNumberOfResponse
-                                            }
-                                            sup_question={sub_question_1}
-                                            question={sub_question_2}
-                                            range={2}
-                                            bloc_id={key_bloc}
-                                            questionIndex={
-                                              key_question +
-                                              key_sub_question_1 +
-                                              key_sub_question_2
-                                            }
-                                            totalCounterBloc={totalCounterBloc}
-                                            blocIndex={blocIndex}
-                                            counter={counter}
-                                            setBlocIndex={setBlocIndex}
-                                            numberOfResponseBloc={
-                                              numberOfResponseBloc
-                                            }
-                                            setNumberOfResponseBloc={
-                                              setNumberOfResponseBloc
-                                            }
-                                          />
-                                        </div>
-                                        {sub_question_2.sub_questions &&
-                                          sub_question_2.sub_questions.length >
-                                            0 &&
-                                          sub_question_2.sub_questions.map(
-                                            (
-                                              sub_question_3,
-                                              key_sub_question_3
-                                            ) => {
-                                              counter += 1;
-                                              return (
-                                                <div key={`range_4_${counter}`}>
-                                                  {" "}
-                                                  <div
-                                                    key={`question_${counter}`}
-                                                    id={`question_${counter}`}
-                                                  >
-                                                    <RenderQuestion
-                                                      totalCounterBloc={
-                                                        totalCounterBloc
-                                                      }
-                                                      counterBlocLimits={
-                                                        counterBlocLimits
-                                                      }
-                                                      numberOfResponse={
-                                                        numberOfResponse
-                                                      }
-                                                      setNumberOfResponse={
-                                                        setNumberOfResponse
-                                                      }
-                                                      sup_question={
-                                                        sub_question_2
-                                                      }
-                                                      question={sub_question_3}
-                                                      range={3}
-                                                      bloc_id={key_bloc}
-                                                      questionIndex={
-                                                        key_question +
-                                                        key_sub_question_1 +
-                                                        key_sub_question_2 +
-                                                        key_sub_question_3
-                                                      }
-                                                      blocIndex={blocIndex}
-                                                      counter={counter}
-                                                      setBlocIndex={
-                                                        setBlocIndex
-                                                      }
-                                                      numberOfResponseBloc={
-                                                        numberOfResponseBloc
-                                                      }
-                                                      setNumberOfResponseBloc={
-                                                        setNumberOfResponseBloc
-                                                      }
-                                                    />
-                                                  </div>
-                                                </div>
-                                              );
-                                            }
-                                          )}
-                                      </div>
-                                    );
-                                  }
-                                )}
-                            </div>
-                          );
-                        }
-                      )}
-                  </div>
-                );
-              })}
+              
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
       <Bg />
--- src/components/NewForm/RenderQuestion.js
+++ src/components/NewForm/RenderQuestion.js
@@ -1,96 +1,25 @@
 import RenderInput from "./RenderInput";
 import sendIcon from "../../assets/svg/send.svg";
-import { useState, useReducer, useEffect } from "react";
 
 const RenderQuestion = ({
   question,
-  questionIndex,
   numberOfResponse,
   setNumberOfResponse,
   userValues,
-  counterBlocLimits,
-  range,
-  bloc_id,
-  blocIndex,
-  counter,
-  setBlocIndex,
-  totalCounterBloc,
-  numberOfResponseBloc,
-  setNumberOfResponseBloc,
-  sup_question,
+  hasResponse,
 }) => {
-  const [blocs, allBlocs] = useState([]);
-  const [questions, allQuestions] = useState([]);
-  const [asks, allAsks] = useState([]);
-  const [sendInputs, allSendInputs] = useState([]);
-  const [responses, allResponses] = useState([]);
-  const [displayButtonNextBloc, setDisplayButtonNextBloc] = useState(false);
-  const [displayQuestion, setDisplayQuestion] = useState("is-hidden");
-  // useEffect(() => {
-  //   if()
-  // }, []);
-  console.log(displayQuestion);
-  const [numberOfResponseBlocMax, setNumberOfResponseBlocMax] = useState(
-    counterBlocLimits.find(
-      (element) => Number(element.id.split("bloc_")[1]) === bloc_id
-    ) &&
-      counterBlocLimits.find(
-        (element) => Number(element.id.split("bloc_")[1]) === bloc_id
-      ).counter
-  );
-  useEffect(() => {
-    if (totalCounterBloc === numberOfResponseBlocMax) {
-      setDisplayButtonNextBloc(true);
-    }
-  }, [totalCounterBloc, numberOfResponse]);
-  useEffect(() => {
-    allBlocs(Array.from(document.querySelectorAll('[id^="bloc_"]')));
-    allQuestions(Array.from(document.querySelectorAll('[id^="question_"]')));
-    allAsks(Array.from(document.querySelectorAll('[id^="ask_"]')));
-    allSendInputs(Array.from(document.querySelectorAll('[id^="send_input_"]')));
-    allResponses(Array.from(document.querySelectorAll('[id^="response_"]')));
-  }, []);
-  useEffect(() => {
-    blocs.map((element) =>
-      Number(element.id.split("bloc_")[1]) <= blocIndex
-        ? element.classList.remove("is-hidden")
-        : element.classList.add("is-hidden")
-    );
-  }, [blocs]);
-  useEffect(() => {
-    questions.map((element) =>
-      Number(element.id.split("question_")[1]) <= numberOfResponse + 1
-        ? element.classList.remove("is-hidden")
-        : element.classList.add("is-hidden")
-    );
-    asks.map((element) =>
-      Number(element.id.split("ask_")[1]) <= numberOfResponse + 1
-        ? element.classList.remove("is-hidden")
-        : element.classList.add("is-hidden")
-    );
-    responses.map((element) =>
-      Number(element.id.split("response_")[1]) < numberOfResponse + 1
-        ? element.classList.remove("is-hidden")
-        : element.classList.add("is-hidden")
-    );
-    sendInputs.map((element) =>
-      Number(element.id.split("send_input_")[1]) === numberOfResponse + 1
-        ? element.classList.remove("is-hidden")
-        : element.classList.add("is-hidden")
-    );
-  }, [questions, numberOfResponse]);
+  console.log(question.index_id);
+  let question_id = Number(question.question_id.split("-")[1].split("_")[1]);
   return (
-    <div className={displayQuestion ? "" :"is-hidden"}>
-      <div
-        className={numberOfResponse <= totalCounterBloc ? "ask" : "is-hidden"}
-        id={`ask_${counter}`}
-      >
+    <>
+      <div id={`ask_${question_id}`} className="ask">
         <p>{question.question}</p>
       </div>
 
       <div
         style={{ display: "flex", flexDirection: "column" }}
-        id={`response_${counter}`}
+        id={`response_${question_id}`}
+        className={numberOfResponse >= question.index_id + 1 ? "" : "is-hidden"}
       >
         {question.formInput.label && (
           <div className="response ">
@@ -107,7 +36,10 @@ const RenderQuestion = ({
           </div>
         </div>
       </div>
-      <div className={"inputField"} id={`send_input_${counter}`}>
+      <div
+        className={hasResponse ? "is-hidden" : "inputField"}
+        id={`send_input_${question_id}`}
+      >
         <div className="response-input">
           <RenderInput formInput={question.formInput} userValues={userValues} />
         </div>
@@ -115,34 +47,11 @@ const RenderQuestion = ({
           className="btn"
           onClick={() => {
             setNumberOfResponse(numberOfResponse + 1);
-            setNumberOfResponseBloc(numberOfResponseBloc + 1);
-            // console.log({ numberOfResponse }, { totalCounterBloc });
           }}
         >
           <img src={sendIcon} alt=""></img>
         </button>
       </div>
-      {displayButtonNextBloc && (
-        <div
-          className={
-            totalCounterBloc === numberOfResponse ? "inputField" : "is-hidden"
-          }
-        >
-          {" "}
-          <button
-            className="btn"
-            onClick={() => {
-              setBlocIndex(blocIndex + 1);
-              setDisplayButtonNextBloc(false);
-              // setNumberOfResponseBloc(0);
-
-              // setNumberOfResponse(numberOfResponse + 1);
-            }}
-          >
-            Passer au bloc suivant {totalCounterBloc} {numberOfResponse}
-          </button>
-        </div>
-      )}
     </>
   );
 };
