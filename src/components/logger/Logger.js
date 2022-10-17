6520a2f7f530efb34510f5ea01f6827658f8a5af
bertillesmith <bertilled.smith@gmail.com>
2022-10-17 08:43:02 -0400

    correct a typo in logger.js and change url in login.js

--- src/components/logger/Logger.js
+++ src/components/logger/Logger.js
@@ -1,34 +1,43 @@
 import * as React from "react";
-import './Logger.css';
+import "./Logger.css";
 import { Link } from "react-router-dom";
 import Lottie from "lottie-react";
-import fandr from '../../assets/anim/fandr.json';
-import home from '../../assets/svg/home.svg';
+import fandr from "../../assets/anim/fandr.json";
+import home from "../../assets/svg/home.svg";
 
 function Logger() {
   const noNav = `.dash-nav, .nav-margin, .dash-side {
     display: none !important;
-}`
-    return (
-      <>
+}`;
+  return (
+    <>
       <style>{noNav}</style>
-        <div className="navbar">
-          <Link to={"../"}><img src={home}></img></Link>
+      <div className="navbar">
+        <Link to={"../"}>
+          <img src={home}></img>
+        </Link>
+      </div>
+      <div id="logger">
+        <Lottie animationData={fandr} loop={true} />
+        <div className="Titles">
+          <h1>Take part of the project.</h1>
+          <p>
+            We really appreciate your commitment, and we are glad that you're
+            interested in our project. <br></br>But before going further, do we
+            already know each other?
+          </p>
         </div>
-        <div id="logger">
-            <Lottie animationData={fandr} loop={true}/>
-            <div className="Titles">
-                <h1>Take part of the project.</h1>
-                <p>We really appreciate your commitment, and we are glade that you're interested in our project. <br></br>But before going further, do we already know each other?</p>
-            </div>
-            <div className="choice">
-              <Link to={"register"} className="register">Register</Link>
-              <Link to={"login"} className="signin">Sign in</Link>
-            </div>
+        <div className="choice">
+          <Link to={"register"} className="register">
+            Register
+          </Link>
+          <Link to={"login"} className="signin">
+            Sign in
+          </Link>
         </div>
-      </>
-    );
-  }
-  
-  export default Logger;
-  
+      </div>
+    </>
+  );
+}
+
+export default Logger;
